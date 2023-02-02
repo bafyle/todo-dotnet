81ebfed572e0c23f2d12d4496a54872a9c9b6699
Andrew <androw.emad@yahoo.com>
2023-02-02 23:39:29 +0200

    added user registeration

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -11,8 +11,8 @@ public class UsersController : Controller
 {
 
     private readonly AppDbContext db;
-    private SignInManager<IdentityUser> _signManager; 
-    private UserManager<IdentityUser> _userManager;  
+    private SignInManager<IdentityUser> _signManager;
+    private UserManager<IdentityUser> _userManager;
     public UsersController(AppDbContext appDb, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
     {
         db = appDb;
@@ -41,6 +41,45 @@ public class UsersController : Controller
             return View(data); 
         }
         return RedirectToAction("Index", "Tasks");
+    }
+
+    [HttpGet]
+    [Route("/register")]
+    public IActionResult Register()
+    {
+        if(HttpContext.User?.Identity?.IsAuthenticated == true)
+            return RedirectToAction("Index", "Tasks");
+        return View(new UserRegistrationModel());
+    }
+
+    [HttpPost]
+    [Route("/register")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(UserRegistrationModel model)
+    {
+        if(!ModelState.IsValid)
+        {
+            Console.Write("Model state error");
+            ModelState.AddModelError("error", "Something is wrong please enter valid values");
+            return View(model);
+        }
+        var newUser = new IdentityUser
+        {
+            UserName=model.Email,
+            Email=model.Email
+        };
+        newUser.EmailConfirmed = true;
+        var result = await _userManager.CreateAsync(newUser, model.Password);
+        if(!result.Succeeded)
+        {
+            foreach(var error in result.Errors)
+            {
+                Console.Write(error.Description);
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(model);
+        }
+        return RedirectToAction("Login", "users");
     }
 
 }
